46475575f8367671e8fa674115cb82ae4e223150
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2024-01-18 17:53:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Lottery from './Lottery';
+
+import TodoList from './TodoList';
+import Todo from './component/Todo';
+function App() {
+ 
+
+  return (
+    <>
+    {/* <Lottery/> */}
+     <Todo/>
+    </>
+  )
+}
+
+export default App
